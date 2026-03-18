e0b2df642b889a2130d36507fb6bb681559cf3e3
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2026-03-18 05:25:17 +0000

    Refactor main.cpp to remove unused code and add string copying functionality

--- Lab_CPP_class/main.cpp
+++ Lab_CPP_class/main.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
-#include <stdlib.h>  // atoi (convert string to integer)
+#include <string.h>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout << "Hello world" << endl;
+    // system("dir");  // Executes OS command
 
-    cout << "\n the number of arguments are = " << argc << endl;
+    char dest[5];
+    strcpy(dest, argv[1]);
 
-    int sum = 0;
-    for (int i = 0; i < argc; i++){
-        cout << "\n The argument [" << i << "]" << argv[i] << endl;
-        sum += atoi(argv[i]);
-    }
+    
 
-    cout << "\n The sum of all number passed on argument to main = " << sum << endl;
     return 0;
 }
+
+
+
+
+// ============================================================
+
+// #include <iostream>
+// #include <stdlib.h>  // atoi (convert string to integer)
+// using namespace std;
+
+// int main(int argc, char* argv[]){
+//     cout << "Hello world" << endl;
+
+//     cout << "\n the number of arguments are = " << argc << endl;
+
+//     int sum = 0;
+//     for (int i = 0; i < argc; i++){
+//         cout << "\n The argument [" << i << "]" << argv[i] << endl;
+//         sum += atoi(argv[i]);
+//     }
+
+//     cout << "\n The sum of all number passed on argument to main = " << sum << endl;
+//     return 0;
+// }
 
 // --------------------- output ------------------
 /*
